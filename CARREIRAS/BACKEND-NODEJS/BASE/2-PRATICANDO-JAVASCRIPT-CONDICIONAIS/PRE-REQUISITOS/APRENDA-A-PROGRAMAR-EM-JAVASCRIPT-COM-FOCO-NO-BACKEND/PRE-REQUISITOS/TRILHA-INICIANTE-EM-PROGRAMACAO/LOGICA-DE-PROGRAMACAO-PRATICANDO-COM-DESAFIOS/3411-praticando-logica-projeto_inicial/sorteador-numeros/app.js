867888699af9a8381fbf6d7e83552ba668a287d5
mercuttius <gugagugadinodino@gmail.com>
2026-08-24 17:38:30 -0300

    finalizando o jogo Sorteador De Numero

--- CARREIRAS/BACKEND-NODEJS/BASE/2-PRATICANDO-JAVASCRIPT-CONDICIONAIS/PRE-REQUISITOS/APRENDA-A-PROGRAMAR-EM-JAVASCRIPT-COM-FOCO-NO-BACKEND/PRE-REQUISITOS/TRILHA-INICIANTE-EM-PROGRAMACAO/LOGICA-DE-PROGRAMACAO-PRATICANDO-COM-DESAFIOS/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ CARREIRAS/BACKEND-NODEJS/BASE/2-PRATICANDO-JAVASCRIPT-CONDICIONAIS/PRE-REQUISITOS/APRENDA-A-PROGRAMAR-EM-JAVASCRIPT-COM-FOCO-NO-BACKEND/PRE-REQUISITOS/TRILHA-INICIANTE-EM-PROGRAMACAO/LOGICA-DE-PROGRAMACAO-PRATICANDO-COM-DESAFIOS/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -3,28 +3,54 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if(de >= ate){
+        alert('O valor do campo "Do número" não pode ser maior que ou igual ao valor do campo "Até o número"')
+        return;
+    }
+
+    if(quantidade > ate - de + 1){
+        alert('A "Qantidade de números" não pode ser maior que o intervalo entre os valores inseridos nos campos "Do número" e "Até o número"')
+        return;
+    }
+
     let sorteados = [];
     let numero;
 
     for(let i = 0 ; i < quantidade ; i++){
         numero = obterNumeroAleatorio(de, ate)
         while(sorteados.includes(numero)){
-            
+            numero = obterNumeroAleatorio(de, ate);
         }
         sorteados.push(numero)
     }
 
     let resultado = document.getElementById('resultado')
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado')
+        botao.classList.add('container__botao')
+    }else{
+        botao.classList.remove('container__botao')
+        botao.classList.add('container__botao-desabilitado')
+    }
+}
 
-
-
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarStatusBotao
+}
 
 
 
